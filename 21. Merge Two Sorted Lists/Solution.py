6064ac435f52e9fc05743ad58d2119674afa0b8a
Hunaid Khan <hunaid20042004@gmail.com>
2024-08-22 10:42:28 -0600

    Python3 Solution
            Submission Statistics:
            Question #: 21
            Language: Python3
            Runtime: 37 ms
            Runtime percentile: 64.99
            Memory: 16.464 MB
            Memory percentile: 76.28

--- 21. Merge Two Sorted Lists/Solution.py
+++ 21. Merge Two Sorted Lists/Solution.py
@@ -5,14 +5,40 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = temp = ListNode(0)
-        while list1 != None and list2 != None: #1
-            if list1.val < list2.val: #2
-                temp.next = list1 #3
-                list1 = list1.next #4
-            else: 
-                temp.next = list2
-                list2 = list2.next
-            temp = temp.next
-        temp.next = list1 or list2  #5
-        return dummy.next #6
+        #in place sorting
+        #check for lower head value
+        if not list1: return list2
+        if not list2: return list1
+        if list1.val > list2.val:
+            list1, list2 = list2, list1 #swapping so list1 is always smaller head at first
+        if not list1.next:
+            list1.next = list2
+            return list1
+        curr1, curr2 = list1, list2
+        while curr2 and curr1.next:
+            if curr2.val >= curr1.val and curr2.val <= curr1.next.val:
+                #swapping
+                temp = curr1.next 
+                curr1.next = curr2
+                newCurr2 = curr2.next
+                curr2.next = temp
+                #moving pointers ahead
+                curr2 = newCurr2
+                curr1 = curr1.next
+            else:
+                if curr1.next.next:
+                    curr1 = curr1.next
+                else:
+                    curr1.next.next = curr2
+                    return list1
+        return list1
+      
+            
+
+1-2-3-4-4-4
+6
+curr1=4
+curr2=6
+
+        
+        
